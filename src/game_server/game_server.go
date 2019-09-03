8db9319b4c51be90c3ed23e561a2d86080b37c18
Samuel Davidson <samdamana@gmail.com>
2019-09-02 21:50:57 -0700

    Restructured proto imports to be absolute

---
+++ src/game_server/game_server.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	pb "github.com/samdamana/community-chess/src/proto/services/game_server"
+)
+
+type server struct {
+	f pb.
+}
---
+++ src/game_server/main.go
@@ -0,0 +1,12 @@
+// Package main implements a server for the Game Server service.
+package main
+
+import "flag"
+
+var (
+	port = flag.Int("port", 50051, "port the Game Server is accepts connections")
+)
+
+func main() {
+
+}
